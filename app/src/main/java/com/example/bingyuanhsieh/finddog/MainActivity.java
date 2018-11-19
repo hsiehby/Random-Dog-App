4cac6a7005fc40a33b00ed0b2995f4cace94f0a3
Bing-Yuan Hsieh <hsiehby@gmail.com>
2018-11-18 19:35:03 -0500

    Find Dog App ver 0.1

---
+++ app/src/main/java/com/example/bingyuanhsieh/finddog/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.bingyuanhsieh.finddog;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "FindDog:Main";
+
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        // VERY IMPORTANT TO INITIALIZE REQUESTQUEUE!!!
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //display.setImageResource(R.drawable.dog);
+
+        Button button = (Button) findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*String src = getURL();
+                Bitmap newImg = getBitmapFromURL(src);
+                if (newImg != null) {
+                    display.setImageBitmap(newImg);
+                }
+                */
+                startAPICall();
+            }
+        });
+
+
+    }
+
+    // download image
+    public Bitmap getBitmapFromURL(String src) {
+        try {
+            java.net.URL url = new java.net.URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://random.dog/woof.json",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            final ImageView display = findViewById(R.id.imageView1);
+                            Bitmap img = null;
+                            try {
+                                img = getBitmapFromURL(response.get("url").toString());
+                            } catch (JSONException ignored) { }
+                            display.setImageBitmap(img);
+                            apiCallDone(response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            jsonObjectRequest.setShouldCache(false);
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handle the response from our IP geolocation API.
+     *
+     * @param response response from our IP geolocation API.
+     */
+    void apiCallDone(final JSONObject response) {
+        try {
+            Log.d(TAG, response.toString(2));
+            // Example of how to pull a field off the returned JSON object
+            Log.i(TAG, response.get("url").toString());
+        } catch (JSONException ignored) { }
+    }
+
+}
